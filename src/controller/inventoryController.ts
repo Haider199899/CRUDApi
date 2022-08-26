bee5103795a4742e25af27812a661ef38b11d0fd
haider khan <khanshahab3855@gmail.com>
2022-08-26 11:48:14 +0500

    third commit

--- src/controller/inventoryController.ts
+++ src/controller/inventoryController.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import Inventory, { IInventory } from "../model/inventory.model";
 import express ,{Request,Response} from 'express';
+import asyncHandler from 'express-async-handler';
+import Product from "../model/products.model";
 
 const addInventory=(req:Request,res:Response)=>{
     const inventoryData: IInventory = req.body;
@@ -24,7 +26,47 @@ const addInventory=(req:Request,res:Response)=>{
       });
     });
 };
+//update inventory
+const updateInventory = asyncHandler(async (req: Request, res: Response) => {
+    const { product_id } = req.params as { product_id: string };
+    const { name, description, brand, category,price,countInStock}=
+      req.body as {
+        name: string;
+        price:number;
+        countInStock:number;
+        
+        description: string;
+        brand: string;
+        category: string;
+       
+      
+      };
+  
+    const product = await Product.findOneAndUpdate({product_id});
+  
+    if (product) {
+      product.name = name;
+     
+      product.description = description;
+  
+      product.brand = brand; 
+      product.category = category;
+      product.inventory="630756a8794cc7fe16312e88";
+      
+    
+      
+  
+      const updatedProduct = await product.save();
+      res.status(201).json(updatedProduct);
+    } else {
+      res.status(404);
+      throw new Error("Product not found.");
+    }
+  });
+
+
    export {
-    addInventory
+    addInventory,
+    updateInventory
    } ;
 
